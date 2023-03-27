e46f18d55a4eba3de7762125523cc810fb78cb7c
Rayhana E <florayana97@hotmail.com>
2023-03-27 16:18:30 +0200

    created components

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function HomePage() {
+
+}
+
+
+export default HomePage
---
+++ src/components/MyPages.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function MyPages() {
+ 
+}
+
+
+export default MyPages
