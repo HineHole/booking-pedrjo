ddcb26b743e442e46dc27c3446cd08d1a8a2afbe
Elliot Cuhrt <ellcuh@gmail.com>
2023-03-28 11:34:41 +0300

    kopplade komponenter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import './App.css';  
-import Header from './components/Header';
-import HomePage from './components/HomePage';
-import MyPages from './components/MyPages';
-import './App.css';
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import MyPages from "./components/MyPages";
+import Footer from "./components/Footer";
+import Interfaces from "./components/interfaces";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-
-
-
- function App() {
+function App() {
   return (
-    <div className='App'>
-      <Header/>
-      <BrowserRouter>
-
-      
-      </BrowserRouter>
-       
+    <div className="App">
+      <Header />
+      <BrowserRouter></BrowserRouter>
     </div>
-  )
+  );
 }
-
 
 export default App;
