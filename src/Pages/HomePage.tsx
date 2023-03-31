8091f45e79ed3cf738ea748a75df2d8ee2c3d6d4
Linn <linnvailt@gmail.com>
2023-03-31 09:57:17 +0200

    Home Page
    Home Page, link to Customer inactive tho

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export default function HomePage() {
+const HomePageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f2f2f2;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 24px;
+`;
+
+const Subtitle = styled.p`
+  font-size: 24px;
+  color: #666;
+  margin-bottom: 48px;
+`;
+
+const Button = styled.button`
+  padding: 16px 32px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+  background-color: #333;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #666;
+  }
+`;
+
+const HomePage: React.FC = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <HomePageContainer>
+      <Title>Städa Fint AB</Title>
+      <Subtitle>Your home cleaning service</Subtitle>
+      <Button>Get Started</Button>
+    </HomePageContainer>
+  );
+};
+
+export default HomePage;
+
